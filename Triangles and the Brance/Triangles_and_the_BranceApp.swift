684ed780f6bdff0fd2c58d5f049b2442eee4abb1
mori takuminn <mori0116takuminn@gmail.com>
2022-03-04 18:50:48 +0900

    Initial Commit

---
+++ Triangles and the Brance/Triangles_and_the_BranceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Triangles_and_the_BranceApp.swift
+//  Triangles and the Brance
+//
+//  Created by 森本拓未 on 2022/03/04.
+//
+
+import SwiftUI
+
+@main
+struct Triangles_and_the_BranceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
