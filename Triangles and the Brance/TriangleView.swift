b47f693e81225ca3eb25b808ad588ca9c67afaee
mori takuminn <mori0116takuminn@gmail.com>
2022-03-08 11:32:32 +0900

    アニメーションの実装に成功

--- Triangles and the Brance/TriangleView.swift
+++ Triangles and the Brance/TriangleView.swift
@@ -12,7 +12,9 @@ import SwiftUI
 
 struct TriangleView:View, TriCoodinatable,Animatable{
     @ObservedObject var stage:StageModel
-    @State var offset: CGFloat = 3
+    var offset: CGFloat{
+        stage.triangles[indexOfStage].isOn ? 3 : -10
+    }
     
     var coordinate:TriCoordinate
     var length:CGFloat
@@ -20,84 +22,17 @@ struct TriangleView:View, TriCoodinatable,Animatable{
     var indexOfStage: Int{
         stage.getIndexOfStage(coordinate)!
     }
-//    var coordinates:[CGPoint]{
-//        let coordinates:[CGPoint]
-//        let offsetA = offset * sqrt(3)/2 //offset * cos(30[deg])
-//        let x = CGFloat(coordinate.0)
-//        let y = CGFloat(coordinate.1)
-//        let lengthY = length * sqrt(3)/2
-//        
-//        //三角形の向きによって計算式を変更
-//        if coordinate.2{
-//            coordinates = [CGPoint(x: (x + y/2) * length + offsetA,
-//                                   y: y * lengthY + offset/2),
-//                           CGPoint(x: ((x+1) + y/2 ) * length - offsetA,
-//                                   y: y * lengthY + offset/2),
-//                           CGPoint(x: (x + (y+1)/2) * length,
-//                                   y: (y+1) * lengthY - offset),
-//                           CGPoint(x: (x + y/2) * length + offsetA,
-//                                   y: y * lengthY + offset/2)]
-//        }else{
-//            coordinates = [CGPoint(x: (x + y/2) * length,
-//                                   y: y * lengthY + offset),
-//                           CGPoint(x: (x + (y+1)/2) * length - offsetA,
-//                                   y: (y+1) * lengthY - offset/2),
-//                           CGPoint(x: ((x-1) + (y+1)/2) * length + offsetA,
-//                                   y: (y+1) * lengthY - offset/2),
-//                           CGPoint(x: (x + y/2) * length,
-//                                   y: y * lengthY + offset),]
-//        }
-//        return coordinates
-//    }
-//    var backgroundCoordinates:[CGPoint]{
-//        //後ろの三角用のレイヤー、offsetを固定値で上書き
-//        let coordinates:[CGPoint]
-//        let offset = 3.0
-//        let offsetA = offset * sqrt(3)/2 //offset * cos(30[deg])
-//        let x = CGFloat(coordinate.0)
-//        let y = CGFloat(coordinate.1)
-//        let lengthY = length * sqrt(3)/2
-//        
-//        //三角形の向きによって計算式を変更
-//        if coordinate.2{
-//            coordinates = [CGPoint(x: (x + y/2) * length + offsetA,
-//                                   y: y * lengthY + offset/2),
-//                           CGPoint(x: ((x+1) + y/2 ) * length - offsetA,
-//                                   y: y * lengthY + offset/2),
-//                           CGPoint(x: (x + (y+1)/2) * length,
-//                                   y: (y+1) * lengthY - offset),
-//                           CGPoint(x: (x + y/2) * length + offsetA,
-//                                   y: y * lengthY + offset/2)]
-//        }else{
-//            coordinates = [CGPoint(x: (x + y/2) * length,
-//                                   y: y * lengthY + offset),
-//                           CGPoint(x: (x + (y+1)/2) * length - offsetA,
-//                                   y: (y+1) * lengthY - offset/2),
-//                           CGPoint(x: ((x-1) + (y+1)/2) * length + offsetA,
-//                                   y: (y+1) * lengthY - offset/2),
-//                           CGPoint(x: (x + y/2) * length,
-//                                   y: y * lengthY + offset),]
-//        }
-//        return coordinates
-//    }
-    
     
     var body:some View{
-   
-            if stage.triangles[indexOfStage].isOn{
-                TriangleViewChild(offset: offset, coordinate: coordinate, length: length)
-                .fill(stage.currentColor.color)
-                .transition(.opacity)
-                .onTapGesture {
-                    withAnimation{
-                   stage.tapped(stage.triangles[indexOfStage])
-                        offset = -30
-                    }
-                
-               
-               
+        
+        TriangleViewChild(offset: offset, coordinate: coordinate, length: length)
+            .fill(stage.currentColor.color)
+            .onTapGesture {
+                withAnimation(.easeOut(duration: 0.5)){
+                    stage.tapped(stage.triangles[indexOfStage])
+                }
             }
-        }
+            .opacity(stage.triangles[indexOfStage].isOn ? 1 :0)
     }
 }
 
@@ -145,5 +80,5 @@ struct TriangleViewChild:Shape,TriCoodinatable{
         }
         return coordinates
     }
-   
+    
 }
